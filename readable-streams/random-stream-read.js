cf02511bd75de2be211be7d29dc5c4e3d980019c
amcereijo <amcereijo@gmail.com>
2017-06-28 21:09:40 +0200

    Update example for readable-stream

--- readable-streams/random-stream-read.js
+++ readable-streams/random-stream-read.js
@@ -27,3 +27,27 @@ randomStream
       console.log(`[Example] => Data received: ${data.toString()}`);
     }
   });
+
+
+// Without classes
+
+// Readable Stream implemetation
+const randomStream2 = new stream.Readable({
+  read(size) {
+    const data = chance.string();
+    console.log(`[RandomStream2] => Pushing data of size: ${data.length}`);
+    this.push(data, 'utf8');
+    if(chance.bool({likelihood: 5})) {
+      this.push(null);
+    }
+  }
+});
+
+// Using implementation
+randomStream2
+  .on('readable', () => {
+    let data;
+    while((data = randomStream.read()) !== null) {
+      console.log(`[Example_randomStream2] => Data received: ${data.toString()}`);
+    }
+  });
